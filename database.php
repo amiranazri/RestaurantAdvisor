6c870575cec956173f1b5d5d7083c663fe67771b
Amira Nazri <anazri@hotmail.com>
2021-02-14 00:13:32 +0200

    Sixth Commit: created css folder to hold all styling files, create an edit form called edit.php - still needs to be connected to control_panel.php with autofilled data. geoFindMe google function added to create map pins of cliens - restaurants to be done next.

--- database.php
+++ database.php
@@ -27,23 +27,25 @@ try {
     r_address VARCHAR(300) NOT NULL,
     r_phone VARCHAR(50) NOT NULL,
     r_edit VARCHAR(600) NOT NULL,
-    r_delete VARCHAR(600) NOT NULL
+    r_delete VARCHAR(300) NOT NULL,
+    r_long VARCHAR(300) NOT NULL,
+    r_lat VARCHAR(300) NOT NULL
     )";
 
     $conn->exec($sql);
     
-    $sql = "INSERT INTO restaurants (r_name, r_address, r_phone, r_edit, r_delete)
+    $sql = "INSERT INTO restaurants (r_name, r_address, r_phone, r_edit, r_delete, r_long, r_lat)
     VALUES 
-    ('Angelo\'s Pizza', 'Spar Centre 93 Randpark Drive, Randburg 2188 South Africa', '+27117921722', '<button class=\"button\" onclick=\"edit(1)\">Edit</button>', '<button class=\"button\" onclick=\"delete(1)\">Delete</button>'),
-    ('Marble Restaurant', 'Trumpet on Keyes Corner 19 Keyes and, Jellicoe Ave, Rosebank, Johannesburg, 2196', '+27105945550', '<button class=\"button\" onclick=\"edit(1)\">Edit</button>', '<button class=\"button\" onclick=\"delete(1)\">Delete</button>'),
-    ('The Whippet Coffee Linden', '34 7th Str And 4th Avenue Linden, Randburg 2195 South Africa', '+27105945550', '<button class=\"button\" onclick=\"edit(1)\">Edit</button>', '<button class=\"button\" onclick=\"delete(1)\">Delete</button>'),
-    ('Fusionista', '14 Rabie Street, Randburg 2194 South Africa', '+27117934796', '<button class=\"button\" onclick=\"edit(1)\">Edit</button>', '<button class=\"button\" onclick=\"delete(1)\">Delete</button>'),
-    ('Balata Restaurant', 'Setperk Street The Fairway Hotel, Spa & Golf Resort, Randburg 2125 South Africa', '+27114788000', '<button class=\"button\" onclick=\"edit(1)\">Edit</button>', '<button class=\"button\" onclick=\"delete(1)\">Delete</button>'),
-    ('La Mama Restaurant', '90 Oxford Street Ferndale, Randburg 2194 South Africa', '+27763939836', '<button class=\"button\" onclick=\"edit(1)\">Edit</button>', '<button class=\"button\" onclick=\"delete(1)\">Delete</button>'),
-    ('Nando\'s Banbury Drive Thru', 'Olievenhout Avenue Shop B1, Northview Centre, Randburg 2162 South Africa', '+27117949774', '<button class=\"button\" onclick=\"edit(1)\">Edit</button>', '<button class=\"button\" onclick=\"delete(1)\">Delete</button>'),
-    ('The Local Grill', '7th 3rd Avenue Parktown North, Johannesburg 2193 South Africa', '+27118801946', '<button class=\"button\" onclick=\"edit(1)\">Edit</button>', '<button class=\"button\" onclick=\"delete(1)\">Delete</button>'),
-    ('1920 Portuguese Restaurant', '1 Hutton Court Cnr Jan Smuts & Summit Road, Randburg 2196 South Africa', '+27113263161', '<button class=\"button\" onclick=\"edit(1)\">Edit</button>', '<button class=\"button\" onclick=\"delete(1)\">Delete</button>'),
-    ('Yung Chen Craighall', '339 Jan Smuts Avenue Shepherd Market, Craighall, Randburg 2196 South Africa', '+27113254899', '<button class=\"button\" onclick=\"edit(1)\">Edit</button>', '<button class=\"button\" onclick=\"delete(1)\">Delete</button>')";
+    ('Angelo\'s Pizza', 'Spar Centre 93 Randpark Drive, Randburg 2188 South Africa', '+27117921722', '<button class=\"button\" onclick=\"edit(1)\">Edit</button>', '<button class=\"button\" onclick=\"delete(1)\">Delete</button>', '27.942560', '-26.083370'),
+    ('Marble Restaurant', 'Trumpet on Keyes Corner 19 Keyes and, Jellicoe Ave, Rosebank, Johannesburg, 2196', '+27105945550', '<button class=\"button\" onclick=\"edit(1)\">Edit</button>', '<button class=\"button\" onclick=\"delete(1)\">Delete</button>', '28.036489', '-26.144100'),
+    ('The Whippet Coffee Linden', '34 7th Str And 4th Avenue Linden, Randburg 2195 South Africa', '+27105945550', '<button class=\"button\" onclick=\"edit(1)\">Edit</button>', '<button class=\"button\" onclick=\"delete(1)\">Delete</button>', '28.036489', '-26.144100'),
+    ('Fusionista', '14 Rabie Street, Randburg 2194 South Africa', '+27117934796', '<button class=\"button\" onclick=\"edit(1)\">Edit</button>', '<button class=\"button\" onclick=\"delete(1)\">Delete</button>', '27.989200', '-26.110110'),
+    ('Balata Restaurant', 'Setperk Street The Fairway Hotel, Spa & Golf Resort, Randburg 2125 South Africa', '+27114788000', '<button class=\"button\" onclick=\"edit(1)\">Edit</button>', '<button class=\"button\" onclick=\"delete(1)\">Delete</button>', '27.973480', '-26.119750'),
+    ('La Mama Restaurant', '90 Oxford Street Ferndale, Randburg 2194 South Africa', '+27763939836', '<button class=\"button\" onclick=\"edit(1)\">Edit</button>', '<button class=\"button\" onclick=\"delete(1)\">Delete</button>', '27.979980', '-26.083020'),
+    ('Nando\'s Banbury Drive Thru', 'Olievenhout Avenue Shop B1, Northview Centre, Randburg 2162 South Africa', '+27117949774', '<button class=\"button\" onclick=\"edit(1)\">Edit</button>', '<button class=\"button\" onclick=\"delete(1)\">Delete</button>', '27.924260', '27.924260'),
+    ('The Local Grill', '7th 3rd Avenue Parktown North, Johannesburg 2193 South Africa', '+27118801946', '<button class=\"button\" onclick=\"edit(1)\">Edit</button>', '<button class=\"button\" onclick=\"delete(1)\">Delete</button>', '28.028481', '-26.147770'),
+    ('1920 Portuguese Restaurant', '1 Hutton Court Cnr Jan Smuts & Summit Road, Randburg 2196 South Africa', '+27113263161', '<button class=\"button\" onclick=\"edit(1)\">Edit</button>', '<button class=\"button\" onclick=\"delete(1)\">Delete</button>', '28.033530', '-26.128200'),
+    ('Yung Chen Craighall', '339 Jan Smuts Avenue Shepherd Market, Craighall, Randburg 2196 South Africa', '+27113254899', '<button class=\"button\" onclick=\"edit(1)\">Edit</button>', '<button class=\"button\" onclick=\"delete(1)\">Delete</button>', '28.030550', '-26.123420')";
     
     $conn->exec($sql);
 }
--- init_map.js
+++ init_map.js
@@ -1,10 +1,35 @@
+let map, infoWindow;
+
 function initMap() {
-        
-// The location of Uluru
-const uluru = { lat: -25.344, lng: 131.036 };
-// The map, centered at Uluru
-const map = new google.maps.Map(document.getElementById("map"), {
-  zoom: 4,
-  center: uluru,
-});
+
+  function success(position) {
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const pos = { lat: latitude, lng: longitude };
+
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: pos,
+    });
+    const marker = new google.maps.Marker({
+      position: pos,
+      map: map,
+      icon: {
+        url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+      }
+    });
+    for (i = 0; i <= 1; i++) {
+      const marker = new google.maps.Marker({
+      position: { lat: -26.102640, lng: 27.961170 },
+      map: map,
+      icon: {
+        url: "http://maps.google.com/mapfiles/ms/icons/pink-dot.png"
+      }
+    });
+    }
+  }
+  function error () {
+    console.log("ERROR")
+  }
+  navigator.geolocation.getCurrentPosition(success, error);
 }
